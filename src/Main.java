e015c7f4806969010c6c6e3851acf6c767110239
dogukanakkan <dogukanakkan3@gmail.com>
2023-07-13 16:52:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+ public class Main {
+     public static void main(String[] args) {
+
+          Employee amele1 = new Employee("Yusuf",2000,45,1985);
+          Employee amele2 = new Employee("Tahsin",2000,50,2000);
+          amele1.printInfo();
+          amele2.printInfo();
+
+
+     }
+
+}
+
